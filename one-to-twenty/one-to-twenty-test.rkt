ff4380de161e26d27101c4037edb90b800a348e1
User <paadams@nmu.edu>
2017-07-03 10:54:24 -0400

    added new tests

--- one-to-twenty/one-to-twenty-test.rkt
+++ one-to-twenty/one-to-twenty-test.rkt
@@ -7,9 +7,21 @@
   (test-suite
    "Tests for problems 1 to 20"
 
-   (check-equal? (sum 1 1) 2)
-   
-   (check-equal? (= 1 1) #t)
+   "problem #1"
+   (test-case
+       "Enter a value which will make the form (= __ #t) evaluate to true"
+     (check-eq? #t #t))
+
+   "problem #2"
+   (test-case
+       "Enter enough to fill in the blank (= (- 10 (* 2 3)) __)"
+     (check-eq? (- 10 (* 2 3)) 4))
+
+   "problem #3"
+   (test-case
+       "Enter value which makes form equal (= __ (string-upcase 'hello world'))"
+     (check-equal? (string-upcase "hello world") "HELLO WORLD")
+     )
    ))
 
 (run-tests one-to-twenty-tests)
